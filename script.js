3e2d6fc2ffaa0f41414bff154afe6e0a84db05ad
Gabriel <gab.atienza@gmail.com>
2019-10-02 13:59:43 -0700

    fixed broken links. Updated assets

--- script.js
+++ script.js
@@ -16,18 +16,19 @@ window.onscroll = function () {
 };
 
 var projects = document.getElementsByClassName('project');
+var projectDetails = document.getElementsByClassName('project-details')
 
 for (var i = 0; i < projects.length; i++) {
   projects[i].addEventListener('mouseenter', detailsVisible, false);
 }
 
 for (var i = 0; i < projects.length; i++) {
-  projects[i].addEventListener('mouseleave', detailsInvisible, false);
+  projectDetails[i].addEventListener('mouseleave', detailsInvisible, false);
 }
 
 function detailsVisible(e) {
-  e.target.classList.add("details-visible");
-  e.target.classList.remove("details-invisible");
+  e.target.children[1].classList.add("details-visible");
+  e.target.children[1].classList.remove("details-invisible");
 };
 
 function detailsInvisible(e) {
